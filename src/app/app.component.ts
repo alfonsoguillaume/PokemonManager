ad91e2c571c4a6ad78cdad32be77db22293151c6
Guillaume Alfonso <guillaume.alfonso2026@campus-eni.fr>
2026-07-04 14:24:00 +0200

    first try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GenerationsComponent } from "./composant/generations/generations.component";
+import { PokemonsComponent } from "./composant/pokemons/pokemons.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GenerationsComponent],
+  imports: [RouterOutlet, GenerationsComponent, PokemonsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/composant/generations/generations.component.ts
+++ src/app/composant/generations/generations.component.ts
@@ -1,12 +1,59 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';   // <--- AJOUTE CETTE LIGNE
+
 
 @Component({
   selector: 'app-generations',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterLink],
   templateUrl: './generations.component.html',
   styleUrl: './generations.component.css'
 })
-export class GenerationsComponent {
 
+export class GenerationsComponent {
+  public generations =[
+    { id: 1, name: 'Génération I' },
+    { id: 2, name: 'Génération II' },
+    { id: 3, name: 'Génération III' },
+    { id: 4, name: 'Génération IV' },
+    { id: 5, name: 'Génération V' },
+    { id: 6, name: 'Génération VI' },
+    { id: 7, name: 'Génération VII' },
+  ]
+
+  /*constructor() {
+    this.generations = []
+  }
+
+  addGenerations() {
+    this.generations.push({
+      id:1,
+      name: 'Generation I'
+    });
+    this.generations.push({
+      id:2,
+      name: 'Generation II'
+    });
+    this.generations.push({
+      id:3,
+      name: 'Generation III'  
+    });
+    this.generations.push({
+      id:4,
+      name: 'Generation IV'
+    });
+    this.generations.push({
+      id:5,
+      name: 'Generation V'
+    });
+    this.generations.push({
+      id:6,
+      name: 'Generation VI' 
+    });
+    this.generations.push({
+      id:7,
+      name: 'Generation VII'
+    });
+}*/
 }
